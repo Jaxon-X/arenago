7d856377476bc64626a4d11ef9d8038160b2e34e
Jaxon-X <xudoyberdiyevajahon8@gmail.com>
2025-12-06 00:16:19 +0500

    feat(stadiums): Add IsObjectOwner permission for API security

---
+++ stadiums/permissions.py
@@ -0,0 +1,12 @@
+
+from rest_framework import permissions
+from rest_framework.permissions import SAFE_METHODS
+
+class IsObjectOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+
+        if request.method in SAFE_METHODS:
+            return True
+
+        return request.user == obj.owner
+
