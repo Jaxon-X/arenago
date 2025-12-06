f7ce5842d7d699ed5931f6c5c1b69665061b84e2
Jaxon-X <xudoyberdiyevajahon8@gmail.com>
2025-12-06 20:47:12 +0500

    feat(stadiums): Implement CRUD views and spatial nearest neighbor search

--- stadiums/views.py
+++ stadiums/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.gis.geos import Point
+from django.contrib.gis.db.models.functions import Distance
 
-# Create your views here.
+from .models import Stadium
+from .serializers import StadiumSerializer
+from .permissions import IsObjectOwner
+
+
+class StadiumListCreateView(generics.ListCreateAPIView):
+    queryset = Stadium.objects.all()
+    serializer_class = StadiumSerializer
+    permission_classes = [IsAuthenticated, IsObjectOwner]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+
+class StadiumDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Stadium.objects.all()
+    serializer_class = StadiumSerializer
+    permission_classes = [IsAuthenticated, IsObjectOwner]
+
+
+class StadiumNearByView(generics.ListAPIView):
+    serializer_class = StadiumSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        lat = self.request.query_params.get('lat')
+        lon = self.request.query_params.get('lon')
+
+        if lat and lon:
+            try:
+                latitude = float(lat)
+                longitude = float(lon)
+
+                user_location = Point(latitude, longitude, srid=4326)
+
+                return Stadium.objects.annotate(
+                    distance = Distance('location', user_location)).order_by('distance')
+            except ValueError:
+                return Stadium.objects.all()
+
+        return Stadium.objects.all()
+
