223275f383f3145e4d5903cb927006d13e3c6e79
Jaxon-X <xudoyberdiyevajahon8@gmail.com>
2025-12-10 23:04:39 +0500

    feat(stadium): add view for filter stadium by price and distance

--- stadiums/views.py
+++ stadiums/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from django.contrib.gis.geos import Point
 from django.contrib.gis.db.models.functions import Distance
@@ -26,12 +27,12 @@ class StadiumListCreateView(generics.ListCreateAPIView):
 class StadiumDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Stadium.objects.all()
     serializer_class = StadiumSerializer
-    permission_classes = [IsAuthenticated, IsObjectOwner]
+    permission_classes = [AllowAny, IsObjectOwner]
 
 
 class StadiumNearByView(generics.ListAPIView):
     serializer_class = StadiumListSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [AllowAny] #keyin isAuthenticatedga o'zgartirish kerak
 
     def get_queryset(self):
         lat = self.request.query_params.get('lat')
@@ -50,4 +51,48 @@ class StadiumNearByView(generics.ListAPIView):
                 return Stadium.objects.none()
 
         return Stadium.objects.all()
+
+
+class StadiumViewByFilter(generics.ListAPIView):
+    serializer_class = StadiumListSerializer
+    permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        queryset = Stadium.objects.all()
+        order_by_field = 'name'
+
+        lat = self.request.query_params.get('lat')
+        lon = self.request.query_params.get('lon')
+
+        if lat and lon:
+            try:
+                latitude = float(lat)
+                longitude = float(lon)
+                user_location = Point(longitude, latitude, srid=4326)
+
+                queryset = queryset.annotate(
+                    distance=Distance('location', user_location)
+                )
+                order_by_field = 'distance'
+
+            except (ValueError, TypeError):
+                raise ValidationError("Kordinatalar formati noto'g'ri. Iltimos, raqam kiriting.")
+
+        min_price = self.request.query_params.get('min_price')
+        max_price = self.request.query_params.get('max_price')
+
+        if min_price is not None and max_price is not None:
+            try:
+                min_price_float = float(min_price)
+                max_price_float = float(max_price)
+
+                queryset = queryset.filter(
+                    price_per_hour__range=(min_price_float, max_price_float)
+                )
+            except (ValueError, TypeError):
+                raise ValidationError("Narx qiymatlari formati noto'g'ri. Iltimos, faqat raqam kiriting.")
+
+        return queryset.order_by(order_by_field)
+
+
 
